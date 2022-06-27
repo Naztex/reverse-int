d6abaf603bc0c96c2df45177a1308ba661617dc2
Naztex <nazteck@yandex.ru>
2022-06-27 18:40:48 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let stringN = n.toString();
+  console.log(stringN);
+  let result = '';
+  for (let i = stringN.length - 1; i !== -1; i--) {
+    if (stringN[i] === '-') {
+        result = result;
+    } else {
+    result += stringN[i];
+    }
+  }
+  return parseInt(result);
 }
